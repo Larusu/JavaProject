11da6a724094e353d9c2045545f383e3da7fa5bc
LD <ldaneliee33@gmail.com>
2025-02-03 15:51:46 +0800

    if/else switch statements

--- Main.java
+++ Main.java
@@ -1,6 +1,20 @@
 public class Main {
-    //hehe try try try!!!!
+    private static int[] numbers = {0, 10, 15, 54, 14, 10, 24, 20};
     public static void main(String[] args){
-        System.out.println("Hello, World");
+        for(int i = 0; i < numbers.length; i++){
+            if(numbers[i] == 10 || numbers[i] == 15){
+            System.out.println("The value " + numbers[i] + " is at index " + i);
+            }
+            else if(numbers[i] != 15){System.out.println("Hindi siya 15. index" + i);}
+            else{System.out.println("NOTHING!!!");}
+        }
+        int day = 3;
+            switch (day){
+                case 1: {System.out.println("Monday"); break;}
+                case 2: {System.out.println("Tuesday"); break;}
+                case 3: {System.out.println("Wednesday"); break;}
+                case 4: {System.out.println("Thursday"); break;}
+                case 5: {System.out.println("Friday"); break;}
+            } 
     }
 }
